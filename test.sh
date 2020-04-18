19185acd49a4c31de969a4892232f2f576e94a8d
Vu Truong <vu.truong@veriksystems.com>
2020-04-18 10:27:07 -0500

    Support sub-command init , start, configure

--- test.sh
+++ test.sh
@@ -2,5 +2,12 @@
 
 #./workspace-rsync.sh -f "helloworld.conf"
 #
-./workspace-rsync.sh --ssh kevin@192.168.0.1 --ssh-key $HOME/.ssh/id_rsa --local $(pwd) --remote /mnt/myProject --diff_time 3
+#./ws-rsync.sh config --ssh kevin@192.168.0.1 --ssh-key $HOME/.ssh/id_rsa --local $(pwd) --remote /mnt/myProject --diff_time 3
+#
+./ws-rsync.sh init
+
+#./ws-rsync.sh run start
+
+
+
 
--- ws-rsync.sh
+++ ws-rsync.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-WORK_DIR=$(pwd)
+CUR_WORK_DIR=$(pwd)
+ProgName=$(basename $0)
+
+DEFAULT_DIFF_TIME=3
 
 mkdir -p $HOME/.ws-rsync/
 
@@ -17,7 +20,6 @@ function usage
     echo "usage: workspace-rsync --ssh keivn@my-sample-server --ssh-key $HOME/.ssh/sample_key --local $(pwd) --remote /mnt/my-project/"
 
     echo "   ";
-    echo "  -f | --config_file      ; your configure file";
     echo "  --ssh                   : Your ssh account kevin@kencancode ";
     echo "  --ssh-key               : your ssh private key absolute path <~/home/kevin/.ssh/id_rsa>";
     echo "  --local                 : local work space dir ~/Project/KEVIN/TestABC ";
@@ -27,7 +29,7 @@ function usage
 }
 
 
-function parse_args
+function parse_conf_args
 {
   # positional args
   args=()
@@ -35,8 +37,8 @@ function parse_args
   # named args
   while [[ "$1" != "" ]]; do
       case "$1" in
-          -f | --config_file )                  cfg_file="$2";  shift;;
           --ssh )                               ssh_acc="$2";   shift;;
+          --ssh-pass )                           ssh_pass="$2";   shift;;
           --ssh-key )                           ssh_key="$2";   shift;;
           --local )                          local_dir="$2";   shift;;
           --remote )                         remote_dir="$2";   shift;;
@@ -46,41 +48,44 @@ function parse_args
       esac
       shift # move to next kv pair
   done
-  # restore positional args
   set -- "${args[@]}"
 }
 
+watch_dir_inotify(){
+    return
+}
+
 watch_dir() {
-    echo watching folder $1/ every $2 secs.
+    echo watching folder ${local_dir} every ${diff_time} secs.
     while [[ true ]]
     do
-        files=`find $1 -type f -newermt "$2 seconds ago"`
+        files=`find $1 -type f -newermt "$diff_time seconds ago"`
         if [[ ${files} != "" ]] ; then
-            docker stop hugo-ascii-runner
-            sleep 1
-            hugo_ascii_docker_run -D
-            sleep 1
-            cp -rf ${ROOT_DIR}/assets ${ROOT_DIR}/static/
-            sleep 1
-            hugo_ascii_docker_run "server -D"
+            echo "${files} changed"
+            echo "run update file to remote side"
         fi
-        sleep 3
+        sleep ${diff_time}
     done
 }
 
+function get_ws_config_file() {
+    ASB_DIR_PATH=${CUR_WORK_DIR}
+    MD5_CUR_DIR=$(echo -n ${ASB_DIR_PATH} | md5sum | awk '{print $1}')
+    echo "$HOME/.ws-rsync/.config_${MD5_CUR_DIR}"
+}
+
 function get_ws_config(){
+    ws_cfg_file=$(get_ws_config_file)
+    echo "Configure file is: $ws_cfg_file"
+    . "${ws_cfg_file}"
     return
 }
 
 function write_ws_config(){
-    ws_cfg_file=$HOME/.ws-rsync/.config_"${PWD##*/}"
+    ws_cfg_file=$(get_ws_config_file)
     echo "ws configure file: $ws_cfg_file"
     echo "clean up the workspace configure file"
     echo  > ${ws_cfg_file}
-
-    if [[ ! -z ${cfg_file} ]]; then
-        echo "(${!cfg_file@})=$cfg_file" >> ${ws_cfg_file}
-    fi
 
     if [[ ! -z ${ssh_acc} ]]; then
         echo "${!ssh_acc@}=$ssh_acc" >> ${ws_cfg_file}
@@ -88,6 +93,10 @@ function write_ws_config(){
 
     if [[ ! -z ${ssh_key} ]]; then
         echo "${!ssh_key@}=$ssh_key" >> ${ws_cfg_file}
+    fi
+
+    if [[ ! -z ${ssh_pass} ]]; then
+        echo "${!ssh_pass@}=$ssh_pass" >> ${ws_cfg_file}
     fi
 
     if [[ ! -z ${local_dir} ]]; then
@@ -106,6 +115,10 @@ function write_ws_config(){
         echo "${!diff_time@}=$diff_time" >> ${ws_cfg_file}
     fi
 
+    # if diff_time is NULL then add default value is 3 secs
+    if [[ -z ${diff_time} ]];then
+        diff_time=${DEFAULT_DIFF_TIME}
+    fi
     return;
 }
 
@@ -113,20 +126,83 @@ function validate_ws_config(){
     return
 }
 
-function run_configue{
-    return;
+function validate_bool_input(){
+    input="${1^^}" # capitalized input
+    if [[ "${input}" == "NO" ]];then
+        echo 0
+    elif [[ "${input}" == "YES" ]];then
+        echo 1
+    else
+        echo -1
+    fi
 }
 
-function run_action_remote_local_sync{
+function input_ssh_authenticate(){
+    read -p 'Using private key to authenticate,default Yes (yes/no)': auth_private_key
+    if [[ -z "${auth_private_key}" ]]; then
+        auth_private_key="yes"
+    fi
+    echo ${auth_private_key}
+    auth_private_key=$(validate_bool_input ${auth_private_key})
+    echo ${auth_private_key}
+    if [[ ${auth_private_key} == -1 ]]; then
+        echo "Error on options using private key or not"
+        auth_private_key=1
+    fi
+    if [[ ${auth_private_key} == 1 ]]; then
+         read -p 'Enter your ssh private key:' ssh_key
+    else
+        read -sp 'Enter your ssh password:' ssh_pass
+    fi
+}
+
+function run_init(){
+    echo "run_init"
+    read -p "Enter your ssh account:" ssh_acc
+    input_ssh_authenticate
+    read -p "Enter your local workspace:" local_dir
+    read -p "Enter your remote workspace:" remote_dir
+    write_ws_config
     return
 }
 
-function run_action_local_remote_sync{
+function run_configue(){
+    ws_cfg_file=$(get_ws_config_file)
+    if [[ -f "$ws_cfg_file" ]]; then
+        . ${ws_cfg_file}
+    fi
+    parse_conf_args "$@"
+    write_ws_config
     return;
 }
 
-function run_action{
+function run_action_remote_local_sync(){
+    echo "run_action_remote_local_sync"
     return
+}
+
+function run_action_local_remote_sync(){
+    echo "run_action_local_remote_sync"
+    return;
+}
+
+function run_start(){
+    echo "run_start"
+    get_ws_config
+    # positional args
+    args=()
+    # named args
+    while [[ "$1" != "" ]]; do
+        case "$1" in
+            -d | --daemon )                  daemon=true;  shift;;
+           -h | --help )                         usage;          exit;; # quit and show usage
+            * )                           args+=("$1")             # if no match, add it to the positional args
+        esac
+        shift # move to next kv pair
+    done
+    # restore positional args
+    set -- "${args[@]}"
+    watch_dir
 }
 
 # ws-rsync.sh init : you will run the initial steps for the current workspace dir"
@@ -135,7 +211,8 @@ function run_action{
 # After inital/configue then can use the script to run some action
 # by default, the script will run the default action monitoring and sync the workspace
 # with daemon options, then script will run in background for example
-# ws-rsync.sh run --daemon  : workspace-rsync will monitor your configured workspace
+# ws-rsync.sh start --daemon  : workspace-rsync will monitor your configured workspace
+# ws-rsync.sh stop : workspace-rsync will monitor your configured workspace
 # when we think that remote side is latest version, then we need to sync up with remote site. In this case, that we will
 # download the newer files from remote workspace
 # ws-rsync.sh run remote-local-sync : run a sync up from local to remote: rsync download newer from remote to local/ scp download-replace everything from remote to local
@@ -143,15 +220,41 @@ function run_action{
 # ws-rsync.sh run local-remote-sync : run a sync up from local to remote: rync  upload the newer from local to remote....
 function run
 {
-  parse_args "$@"
-  #  TODO need to check current dir is already have configure or not.
-  echo "named arg: config_file: $cfg_file"
-  echo "named arg: ssh : $ssh_acc"
-  echo "named arg: ssh-key: $ssh_key"
-  echo "named arg: local: $local_dir"
-  echo "named arg: remote: $remote_dir"
-  echo "named arg: diff_time: $diff_time"
-  write_ws_config
+    subcommand=$1
+    action=$2
+    case ${subcommand} in
+    "" | "-h" | "--help")
+        usage
+        exit 0
+        ;;
+    "" | "init" )
+        shift
+        run_init
+        exit 0
+        ;;
+    "" | "config" )
+        shift
+        run_configue $@
+        exit 0
+        ;;
+    "" | "run")
+    shift
+    action=$1
+    shift
+    echo "run action: ${subcommand}_${action} $@"
+    ${subcommand}_${action} $@
+    if [[ $? = 127 ]]; then
+            echo "Error: '$subcommand' is not a known subcommand." >&2
+            echo "       Run '$ProgName --help' for a list of known subcommands." >&2
+            exit 1
+        fi
+    ;;
+    *)
+        echo "Error: '$subcommand' is not a known subcommand." >&2
+        echo "       Run '$ProgName --help' for a list of known subcommands." >&2
+        exit 1
+        ;;
+esac
 }
 
 run "$@";
